0874aa5067554aca9408e3cc67b20035392261ef
umgefahren <55623006+umgefahren@users.noreply.github.com>
2023-08-04 17:17:04 +0200

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import vercelStatic from "@astrojs/vercel/static";
+import prefetch from "@astrojs/prefetch";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://aink.news",
+  integrations: [tailwind(), prefetch(), sitemap()],
   output: "static",
   adapter: vercelStatic({
     analytics: true,
